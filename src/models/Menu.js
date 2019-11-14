9f3c4c1b6aad74492431334c91fbe4c4ffe2f37a
Soporte <soporte@iMac-de-MacTechlab-1907.local>
2019-11-14 14:21:06 -0600

    Se agrego menu

---
+++ src/models/Menu.js
@@ -0,0 +1,63 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const MenuSchema = new Schema({
+    platillo: {
+        type:String,
+        required:true
+    },
+    ingredinetes: {
+        type:String, 
+        required:true
+    },
+    picante: {
+        type:String,
+        enum: ['Nada', 'Poco', 'Medio', 'Picante', 'Muy Picante']
+    },
+    precio: {
+        type:Number, 
+        require:true
+    },
+    restaurante: {
+        type:String,
+        require:true
+    },
+    img_platillo: {
+        type:String, 
+        require:true
+    },
+    descripcion: {
+        type:String,
+        require:true
+    },
+    carrito: [
+        {
+            type:mongoose.Schema.Types.ObjectID,
+            ref: 'Users'
+        }
+    ]
+},{
+    timestamps:true
+}, { timestamps: true });
+
+const itemsSchema = new Schema ({
+    Nombre: String,
+    addProducts: [],
+    Total: Number
+})
+
+const carritoSchema = new Schema ({
+
+}, { timestamps:true })
+
+const Comida = mongoose.model('Comida', foodSchema);
+const carrito = mongoose.model('carrito', carritoSchema);
+const items = mongoose.model('items', itemsSchema);
+
+module.exports = {
+    Comida, 
+    carrito,
+    items
+}
+
